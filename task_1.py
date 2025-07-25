b1412ca1101a6c1992491ed1cdbbaca86003a824
KorsunAlina <akorsun02@gmail.com>
2025-07-25 15:35:28 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,12 +1,23 @@
 from datetime import datetime, date, timedelta
 
 def get_days_from_today(date):
-    str_to_date=datetime.strptime(date, "%d-%m-%Y").date()
-    today_date=datetime.today().date()
-    if str_to_date>today_date:
-        days_ahead=str_to_date-today_date
+   try:
+    if "." in date: # replace incorrect separator "." for "-"
+        correct_date_format=date.replace(".", "-")
+        str_to_date=datetime.strptime(correct_date_format, "%Y-%m-%d").date()
     else:
-        days_ahead=today_date-str_to_date
-    return days_ahead.days
+            str_to_date=datetime.strptime(date, "%Y-%m-%d").date()
     
-print(get_days_from_today("23-7-2026"))
+    today_date=datetime.today().date()
+    
+    if str_to_date>today_date:
+                days_ahead=str_to_date-today_date
+    else:
+                days_ahead=today_date-str_to_date
+    
+    return days_ahead.days
+   
+   except ValueError: # check incorrect date format
+       return "Incorrect date format."
+    
+print(get_days_from_today("2026,07,25"))
--- task_2.py
+++ task_2.py
@@ -2,33 +2,26 @@ from random import randrange, randint
 
 
 def get_numbers_ticket(min, max, quantity):
+    tickets=set() # return unique values
+    
     if min<1: 
         print("Min value must be greater than 1. Try again.")
-        return
+        return tickets
     
     
     if max>1000: 
-        print("Max value must be lower than 1000. Try again: ")
-        return
+        print("Max value must be lower than 1000. Try again. ")
+        return tickets
      
-    if quantity<min:
-        quantity=print(f"Quantity must be greater than {min}. Try again")
-        return
-        
-    if quantity>max: 
-       print(f"Quantity must be lower than {max}. Try again")
-       return
-       
     
-    tickets=[]  
     for i in range (quantity):
         number=randint(min, max)
-        tickets.append(number)
+        tickets.add(number)
     
     sorted_tickets=sorted(tickets)   
     return sorted_tickets
 
-print(get_numbers_ticket(4, 128, 10))
+print(get_numbers_ticket(3, 113, 5))
         
         
       

cc6f84884126eb117658bbe764d6f12d9b3b63b4
KorsunAlina <akorsun02@gmail.com>
2025-07-27 12:56:55 +0300

    Add files via upload

--- task_2.py
+++ task_2.py
@@ -6,20 +6,20 @@ def get_numbers_ticket(min, max, quantity):
     
     if min<1: 
         print("Min value must be greater than 1. Try again.")
-        return tickets
+        return []
     
     
     if max>1000: 
         print("Max value must be lower than 1000. Try again.")
-        return tickets
+        return []
     
     if min>max:
         print("Min value must be lower than max. Try again.")
-        return tickets
+        return []
     
     if quantity>(max-min):
         print(f"There is not enough values to return {quantity} numbers. Try again.")
-        return tickets
+        return []
          
     
     while len(tickets)<quantity:
@@ -29,7 +29,7 @@ def get_numbers_ticket(min, max, quantity):
     sorted_tickets=sorted(tickets)   
     return sorted_tickets
 
-print(get_numbers_ticket(1, 10, 5)) 
+print(get_numbers_ticket(1, 10000, 5)) 
         
         
       
